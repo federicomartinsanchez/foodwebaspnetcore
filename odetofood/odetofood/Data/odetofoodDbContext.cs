7176a8202d1dbed3c6b1e71da50b2810c33a5a92
Fede <Fede@DESKTOP-1SQSNKK>
2018-01-23 12:21:45 +0100

    Entity Implementation

---
+++ odetofood/odetofood/Data/odetofoodDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using odetofood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace odetofood.Data
+{
+    public class odetofoodDbContext : DbContext
+    {
+        public odetofoodDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
--- odetofood/odetofood/Startup.cs
+++ odetofood/odetofood/Startup.cs
@@ -6,18 +6,28 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using odetofood.Data;
 using odetofood.Services;
 
 namespace odetofood
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContext<odetofoodDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
         }
 
